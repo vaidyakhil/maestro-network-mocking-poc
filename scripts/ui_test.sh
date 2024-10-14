e28539508b735a7e68ab5717186eed20dea6399f
Akhil Vaidya <vaidyakhil@gmail.com>
2024-10-15 01:08:12 +0530

    test: basic ui_test with wiremock webserver stubs and a script to wrap everything together

---
+++ scripts/ui_test.sh
@@ -0,0 +1,44 @@
+# this script assumes that host machine and connected physical device are on the same network
+# else this would not work!
+# getting ip address logic is broken and currently needs to be hardcoded
+
+# helpers
+# # get IP address using 'ip' command
+# get_ip_ip_cmd() {
+#     ip addr show | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}' | cut -d/ -f1
+# }
+
+# # get IP address using 'ifconfig' command
+# get_ip_ifconfig_cmd() {
+#     # impl is broken right now
+#     ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}'
+# }
+
+get_ip() {
+    # # Check if 'ip' command is available
+    # if command -v ifconfig > /dev/null 2>&1; then
+    #     LOCAL_IP_ADDRESS=192.168.0.107
+    # # ifconfig is deprecated and has been replaced by the ip command in most modern Linux distributions
+    # elif command -v ip > /dev/null 2>&1; then
+    #     LOCAL_IP_ADDRESS=$(get_ip_ip_cmd)
+    # else
+    #     echo "Error: Neither 'ip' nor 'ifconfig' command is available."
+    #     exit 1
+    # fi
+    LOCAL_IP_ADDRESS=192.168.0.107
+}
+
+get_ip
+
+DEFAULT_WIREMOCK_SERVER=8080
+MOCK_SERVER_URL="http://$LOCAL_IP_ADDRESS:$DEFAULT_WIREMOCK_SERVER"
+echo "$MOCK_SERVER_URL"
+
+# start wiremock server in background (&)
+(cd wiremock && java -jar wiremock-standalone.jar --verbose &)
+
+maestro test .maestro/flows/quantity_selector.yaml -e MOCK_SERVER_URL=$MOCK_SERVER_URL
+
+# we are done running the tests
+# kill the wiremock server
+curl -X POST -L "$MOCK_SERVER_URL/__admin/shutdown"
---
+++ wiremock/README.md
@@ -0,0 +1,6 @@
+- wiremock standalone jar v3.9.1
+
+- to run local mock server `java -jar wiremock-standalone.jar --verbose`
+
+- to record new api responses, run `java -jar wiremock-standalone.jar --verbose --record-mappings --proxy-all="<actual-base-url>"`
+
